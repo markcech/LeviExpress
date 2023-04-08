cece34234ecfc3fcfe6f6da6e62003e3b7cc88eb
markcech <marketa.sebestova@gmail.com>
2023-04-08 21:33:34 +0200

    Údaje o cestě a výběr města

--- src/components/JourneyPicker/index.jsx
+++ src/components/JourneyPicker/index.jsx
@@ -1,55 +1,94 @@
-import React, { useEffect, useState } from 'react';
-import mapImage from './img/map.svg';
-import './style.css';
+import React, { useEffect, useState } from "react";
+import mapImage from "./img/map.svg";
+import "./style.css";
 
-export const JourneyPicker = ({ onJourneyChange }) => (
-  <div className="journey-picker container">
-    <h2 className="journey-picker__head">Kam chcete jet?</h2>
-    <div className="journey-picker__body">
-      <form className="journey-picker__form">
-        <label>
-          <div className="journey-picker__label">Odkud:</div>
-          <select>
-            <option value="">Vyberte</option>
-            <option value="mesto01">Město 01</option>
-            <option value="mesto02">Město 02</option>
-            <option value="mesto03">Město 03</option>
-            <option value="mesto04">Město 04</option>
-            <option value="mesto05">Město 05</option>
-          </select>
-        </label>
-        <label>
-          <div className="journey-picker__label">Kam:</div>
-          <select>
-            <option value="">Vyberte</option>
-            <option value="mesto01">Město 01</option>
-            <option value="mesto02">Město 02</option>
-            <option value="mesto03">Město 03</option>
-            <option value="mesto04">Město 04</option>
-            <option value="mesto05">Město 05</option>
-          </select>
-        </label>
-        <label>
-          <div className="journey-picker__label">Datum:</div>
-          <select>
-            <option value="">Vyberte</option>
-            <option value="datum01">Datum 01</option>
-            <option value="datum02">Datum 02</option>
-            <option value="datum03">Datum 03</option>
-            <option value="datum04">Datum 04</option>
-            <option value="datum05">Datum 05</option>
-          </select>
-        </label>
-        <div className="journey-picker__controls">
-          <button 
-            className="btn" 
-            type="submit"
-          > 
-            Vyhledat spoj
-          </button>
-        </div>
-      </form>
-      <img className="journey-picker__map" src={mapImage} />
+export const CityOptions = ({ cities }) => {
+  return (
+    <>
+      <option value="">Vyberte</option>
+      {cities.map((city) => (
+        <option key={city.code} value={city.code}>
+          {city.name}
+        </option>
+      ))}
+    </>
+  );
+};
+
+export const JourneyPicker = ({ onJourneyChange }) => {
+  const [fromCity, setFromCity] = useState("");
+  const [toCity, setToCity] = useState("");
+  const [date, setDate] = useState("");
+  const [cities, setCities] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Odesílám formulář s cestou");
+    console.log(fromCity, toCity, date);
+  };
+
+  useEffect(() => {
+    fetch("https://apps.kodim.cz/daweb/leviexpress/api/cities")
+      .then((response) => response.json())
+      .then((data) => setCities(data.results));
+  }, []);
+
+  console.log(cities);
+
+  return (
+    <div className="journey-picker container">
+      <h2 className="journey-picker__head">Kam chcete jet?</h2>
+      <div className="journey-picker__body">
+        <form className="journey-picker__form" onSubmit={handleSubmit}>
+          <label>
+            <div className="journey-picker__label">Odkud:</div>
+            <select
+              value={fromCity}
+              onChange={(e) => {
+                setFromCity(e.target.value);
+              }}
+            >
+              <CityOptions cities={cities} />
+            </select>
+          </label>
+
+          <label>
+            <div className="journey-picker__label">Kam:</div>
+            <select
+              value={toCity}
+              onChange={(e) => {
+                setToCity(e.target.value);
+              }}
+            >
+              <CityOptions cities={cities} />
+            </select>
+          </label>
+
+          <label>
+            <div className="journey-picker__label">Datum:</div>
+            <select
+              value={date}
+              onChange={(e) => {
+                setDate(e.target.value);
+              }}
+            >
+              <option value="">Vyberte</option>
+              <option value="datum01">Datum 01</option>
+              <option value="datum02">Datum 02</option>
+              <option value="datum03">Datum 03</option>
+              <option value="datum04">Datum 04</option>
+              <option value="datum05">Datum 05</option>
+            </select>
+          </label>
+
+          <div className="journey-picker__controls">
+            <button className="btn" type="submit">
+              Vyhledat spoj
+            </button>
+          </div>
+        </form>
+        <img className="journey-picker__map" src={mapImage} />
+      </div>
     </div>
-  </div>
-);
+  );
+};
--- src/index.jsx
+++ src/index.jsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { App } from './components/App';
-import './style.css';
-import { createBrowserRouter } from 'react-router-dom';
-import { Home } from './components/Home';
-import { Reservation } from './components/Reservation';
-import { RouterProvider } from 'react-router-dom';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./components/App";
+import "./style.css";
+import { createBrowserRouter } from "react-router-dom";
+import { Home } from "./components/Home";
+import { Reservation } from "./components/Reservation";
+import { RouterProvider } from "react-router-dom";
 
+export const API_BASE_URL = "https://apps.kodim.cz/daweb/leviexpress/api";
 
-export const API_BASE_URL = 'https://apps.kodim.cz/daweb/leviexpress/api';
-
-const router = createBrowserRouter ([
-    { 
+const router = createBrowserRouter([
+  {
     path: "/",
-    element: <App/>,
+    element: <App />,
     children: [
-        {
-            path: "/",
-            element: <Home/>
-        },
-       
-    ]
-    },
-    {
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
         path: "/reservation",
-        element: <Reservation/>
-    }
-])
+        element: <Reservation />,
+      },
+    ],
+  },
+]);
 
-createRoot(document.querySelector('#app')).render(<RouterProvider router={router} /> );
+createRoot(document.querySelector("#app")).render(
+  <RouterProvider router={router} />
+);
